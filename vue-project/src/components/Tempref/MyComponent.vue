4b7ecaeccdfcb30c0772e16f4cedd8939c3d82e1
nehaumbre <nehaumbre28@gmail.com>
2026-03-04 23:05:26 +0530

    Template ref plus minus example
    Signed-off-by: nehaumbre <nehaumbre28@gmail.com>

--- vue-project/src/components/Tempref/MyComponent.vue
+++ vue-project/src/components/Tempref/MyComponent.vue
@@ -1,18 +1,20 @@
 <script setup>
-import { ref } from 'vue';
+//  it is a way to create a reference to a child component , element or a DOM element within a template
+//which allows you to access and manipulate the referenced object directly in your components logic
+import { ref } from "vue";
 const count = ref(10);
 
-const plus = () => count.value++
-const minus = () => count.value--
+const plus = () => count.value++;
+const minus = () => count.value--;
 
 //script setup is private by default
 // A parent referencing a child cant access anything
 //expose all properties and methods first then we can use it in parent component
-defineExpose({ count, plus, minus })
+defineExpose({ count, plus, minus });
 </script>
 <template>
-    <h1>Ref Component</h1>
-    <p>Count: {{ count }}</p>
-    <button @click="plus">+</button>
-    <button @click="minus">-</button>
+  <h1>Ref Component</h1>
+  <p>Count: {{ count }}</p>
+  <button @click="plus">+</button>
+  <button @click="minus">-</button>
 </template>
